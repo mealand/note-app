439234a57bd25a1b904b1a70701c68c902588fb4
mealand <followmenot@live.com>
2022-09-20 02:50:03 +0100

    UI Development

--- notes-frontend/src/app/app-routing.module.ts
+++ notes-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent, children: [
+    { path: '', component: NoteListComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- notes-frontend/src/app/app.module.ts
+++ notes-frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './pages/note-list/note-list.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
